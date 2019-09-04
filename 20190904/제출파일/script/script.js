00d08cbf123d683318d03a8262653b456f2f606f
junhokyung <junhokyung@naver.com>
2019-09-04 17:53:13 +0900

    0

---
+++ 20190904/제출파일/script/script.js
@@ -0,0 +1,36 @@
+$(function(){
+    $(".main_menu li").mouseover(function(){
+        $(this).find(".sub_menu").stop().slideDown(); 
+    }).mouseout(function(){
+        $(this).find(".sub_menu").stop().slideUp();
+    });
+    
+    var current = 0;
+    var i;
+    
+    function slider(){
+        i=(current+1)%3;
+        
+        $("#banner li").eq(current).fadeOut();
+        $("#banner li").eq(i).fadeIn();
+        
+        current = i;
+    }
+    setInterval(slider,3000);
+    
+    $(".t_menu h2").click(function(){
+        var click = $(this).index();
+        $(".t_menu h2").removeClass("active");
+        $(".t_menu h2").eq(click).addClass("active");
+        
+        $(".t_cont li").removeClass("active");
+        $(".t_cont li").eq(click).addClass("active");
+    });
+    
+    $(".click").click(function(){
+        $(".popup").show();
+    });
+    $(".close").click(function(){
+        $(".popup").hide();
+    });
+});
